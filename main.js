a4aee32a8e87f0548125db5f5681083d2a1df5ee
LevinFX <hp.hogwarts.fan@web.de>
2025-02-23 14:05:39 +0100

    downloadMods Function doesnt quite work yet, but is in the works, about to rewrite it, hopefully it works then.

--- main.js
+++ main.js
@@ -3,6 +3,9 @@ const path = require('path');
 const fs = require('fs');
 const mcl = require('minecraft-launcher-core');
 const { Auth } = require("msmc");
+const {Downloader} = require("nodejs-file-downloader");
+const extract = require('extract-zip');
+const axios = require("axios");
 let mainWindow;
 
 app.whenReady().then(() => {
@@ -39,11 +42,89 @@ app.whenReady().then(() => {
         return {};
     }
 
+    function downloadForge(){
+        console.log("Forge wird heruntergeladen...");
+        const forgeVersion = "1.20.1";
+        const forgePath = path.join(app.getPath('userData'), 'forge_1_20_1.jar');
+        const forgeUrl = `https://maven.minecraftforge.net/net/minecraftforge/forge/1.20.1-47.3.33/forge-1.20.1-47.3.33-installer.jar`;
+        const forgeFile = fs.createWriteStream(forgePath);
+        const request = require('https').get(forgeUrl, (response) => {
+            response.pipe(forgeFile);
+            forgeFile.on('finish', () => {
+                forgeFile.close();
+            });
+        });
+    }
+
+    async function downloadMods() {
+        console.log("Mods werden heruntergeladen...");
+  // URL des Zip-Archivs (direkter Download-Link)
+  const url = "https://www.dropbox.com/scl/fo/k3bgafwk3g8gv20z89je9/AIAh3Z8CR_ssUGh2SBqYg5c?rlkey=sc958rganokob1wz809pudf01&st=ndu2kww6&dl=1";
+  
+  // Pfad, unter dem die Zip-Datei gespeichert wird
+  const zipPath = path.join(__dirname, 'mods.zip');
+  
+  // Zielordner für die entpackten Mods (Projektordner/minecraft/mods)
+  const outputFolder = path.join(__dirname, 'minecraft', 'mods');
+
+  try {
+    console.log('Download der Mods gestartet...');
+
+    // Sicherstellen, dass der Zielordner existiert
+    if (!fs.existsSync(outputFolder)) {
+      fs.mkdirSync(outputFolder, { recursive: true });
+    }
+
+    // Herunterladen der Zip-Datei als Stream
+    const response = await axios({
+      method: 'GET',
+      url: url,
+      responseType: 'stream'
+    });
+
+    // Speichern des Streams in eine Datei
+    const writer = fs.createWriteStream(zipPath);
+    response.data.pipe(writer);
+
+    // Warten bis der Download abgeschlossen ist
+    await new Promise((resolve, reject) => {
+      writer.on('finish', resolve);
+      writer.on('error', reject);
+    });
+
+    console.log('Download abgeschlossen. Entpacke die Mods...');
+
+    console.log("Out: "+outputFolder)
+    console.log("In: "+zipPath)
+    // Entpacken der Zip-Datei in den Zielordner
+    await extract(zipPath, { dir: outputFolder });
+
+    console.log('Mods wurden erfolgreich nach "minecraft/mods" extrahiert.');
+
+    // Löschen der Zip-Datei nach erfolgreicher Extraktion
+    fs.unlink(zipPath, (err) => {
+      if (err) {
+        console.error('Fehler beim Löschen der Zip-Datei:', err);
+      } else {
+        console.log('Zip-Datei wurde gelöscht.');
+      }
+    });
+    
+  } catch (error) {
+    console.error('Fehler beim Herunterladen oder Entpacken der Mods:', error);
+  }
+}
+
     ipcMain.handle('launch-minecraft', () => {
         console.log('Launching Minecraft...');
         const launcher = new mcl.Client();
         const authManager = new Auth("select_account");
         const settingsPath = path.join(app.getPath('userData'), 'settings.json');
+
+        if(!fs.existsSync(path.join(app.getPath('userData'), 'forge.jar'))) {
+            downloadForge();
+        }
+        downloadMods();
 
         if(fs.existsSync(settingsPath)) {
             let settings = JSON.parse(fs.readFileSync(settingsPath, 'utf8'))
@@ -62,7 +143,7 @@ app.whenReady().then(() => {
                     max: settings.ramMax+"M",
                     min: settings.ramMin+"M"
                 },
-                forge: "./minecraft/forge.jar"
+                forge: path.join(app.getPath('userData'), 'forge_1_20_1.jar')
             }
             launcher.launch(launchOpts);
             
